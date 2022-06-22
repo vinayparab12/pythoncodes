69c70c49b1664f50dccb0bbd20675aa5393cd273
Vinay Rajaram Parab <86846642+vinayparab12@users.noreply.github.com>
2022-06-22 23:45:26 +0530

    Create List1andList2Positivenumbers.py

---
+++ List1andList2Positivenumbers.py
@@ -0,0 +1,13 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]  
+print("The Positive numbers of list1 will be ") 
+for num in list1:
+      
+    if num >= 0:
+       print(num, end = " ")
+
+print("\nThe Positive numbers of list2 will be ")
+for num in list2:
+      
+    if num >= 0:
+       print(num, end = " ")       
